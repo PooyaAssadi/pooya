a3b31324d46e3a03fde053edf57f35c706b0bc33
Pooya assadi <99738481+PooyaAssadi@users.noreply.github.com>
2023-07-28 05:26:46 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x8499ee6e8f78aa6bb0240b174ad87b57689b8685";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
